d99d3f581b2630faad4c7136dfe4ad85af6d6aa3
Jayesh <jayeshp2012@gmail.com>
2020-07-26 21:32:57 -0700

    Changed About Me content

--- js/script.js
+++ js/script.js
@@ -87,7 +87,7 @@ particlesJS('particles-js',
             "events": {
                 "onhover": {
                     "enable": true,
-                    "mode": "bubble"
+                    "mode": "grab"
                 },
                 "onclick": {
                     "enable": true,
@@ -97,7 +97,7 @@ particlesJS('particles-js',
             },
             "modes": {
                 "grab": {
-                    "distance": 400,
+                    "distance": 150,
                     "line_linked": {
                         "opacity": 1
                     }
